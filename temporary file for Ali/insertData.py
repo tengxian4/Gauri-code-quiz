12f230c964339fb1d05f0a8a6cc63b4d9d902592
tengxian4 <55143952+tengxian4@users.noreply.github.com>
2021-11-10 09:57:44 +0800

    Update insertData.py

--- temporary file for Ali/insertData.py
+++ temporary file for Ali/insertData.py
@@ -2,10 +2,27 @@ import sqlite3
 
 con = sqlite3.connect('Students.db')
 cur = con.cursor()
-cur.execute("INSERT INTO Student VALUES (1, 'Abu','123')")
 
-cur.execute("INSERT INTO Topic VALUES(1,'Abstract')")
-cur.execute("INSERT INTO Grade VALUES(1,1,60)")
+cur.execute('''CREATE TABLE Topic
+				(Id integer primary key, Name varchar)''')
+
+cur.execute('''CREATE TABLE Question 
+                (Id integer, Question varchar, Selection_A varchar, Selection_B varchar, Selection_C varchar, Selection_D varchar, Answer char, Topic_Id integer, FOREIGN KEY(Topic_Id) REFERENCES Topic(Id))''')
+
+
+#continue Add the topic
+cur.execute("INSERT INTO Topic VALUES(1,'Adjectives and adverbs')")
+
+
+
+#https://www.englisch-hilfen.de/en/exercises/adjectives_adverbs/order.htm
+#Answer must be in upper case
+#Topic Id based on the topic you insert to the Topic table 
+#Choose those exercise that have selection
+cur.execute("INSERT INTO Question VALUES(1,'_________car','a black new','a new black','-','-','B','1')")
+
+
+
 
 
 # Save (commit) the changes
